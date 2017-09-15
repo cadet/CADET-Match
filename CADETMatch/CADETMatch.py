f587539dcc54d59993776c1fc286e1a9a420d157
William Heymann <william.heymann@colorado.edu>
2017-09-15 13:56:58 +0200

    Integrate in fault handler and write out a traceback every 120 seconds. Used to debug

--- CADETMatch/CADETMatch.py
+++ CADETMatch/CADETMatch.py
@@ -4,6 +4,7 @@ import evo
 import grad
 import util
 import time
+import faulthandler
 
 #due to how scoop works and the need to break things up into multiple processes it is hard to use class based systems
 #As a result most of the code is broken up into modules but is still based on pure functions
@@ -17,6 +18,8 @@ def main():
 
 
 if __name__ == "__main__":
+    f = open('cadet_trace', 'w')
+    faulthandler.dump_traceback_later(120, repeat=True, file=f, exit=False)
     start = time.time()
     main()
     print("System has finished")
