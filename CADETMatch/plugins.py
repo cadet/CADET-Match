9410407704106b72c6ac91921ecc0e29c6836b17
William Heymann <william.heymann@colorado.edu>
2018-01-19 13:59:31 +0100

    Removed unneeded functions from plugins

--- CADETMatch/plugins.py
+++ CADETMatch/plugins.py
@@ -12,47 +12,13 @@ def load_plugin(path):
     spec.loader.exec_module(foo)
     return foo
 
-def call_plugin_function(plugin_path, attribute_name, *args, **kwargs):
-    return getattr(load_plugin(plugin_path), attribute_name)(*args, **kwargs)
-
-def call_plugin_by_name(name, directory, attribute_name, *args, **kwargs):
-    plugins = base / directory
-    for path in get_files(plugins):
-        plugin_name = get_plugin_attribute(path, 'name')
-        if plugin_name == name:
-            return call_plugin_function(path, attribute_name, *args, **kwargs)
-
-def call_plugin_by_id(name, attribute_name, *args, **kwargs):
-    module = importlib.import_module(name)
-    return getattr(module, attribute_name)(*args, **kwargs)
-
-def call_plugins_by_name(directory, attribute_name, *args, **kwargs):
-    plugins = base / directory
-    temp = []
-    for path in get_files(plugins):
-        temp.append( call_plugin_function(path, attribute_name, *args, **kwargs) )
-    return temp
-
-def get_attribute_by_name(name, directory, attribute_name):
-    plugins = base / directory
-    for path in get_files(plugins):
-        plugin_name = get_plugin_attribute(path, 'name')
-        if plugin_name == name:
-            return get_plugin_attribute(path, attribute_name)
-
-def get_plugin_attribute(path, attribute_name):
-    return getattr(load_plugin(path), attribute_name)
-
 def get_files(dir):
     return [path for path in dir.glob('*.py') if not path.name == '__init__.py']
-
-def get_plugin_names(directory):
-    plugins = base / directory
-    return [get_plugin_attribute(path, 'name') for path in get_files(plugins)]
 
 def get_plugins(directory):
     plugins = base / directory
     temp = {}
     for path in get_files(plugins):
-        temp[get_plugin_attribute(path, 'name')] = load_plugin(path)
+        plug = load_plugin(path)
+        temp[plug.name] = plug
     return temp
