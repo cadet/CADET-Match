fixed a problem with graphs drawing all units involved correctly when more than one unit is involved smooth experimental data before using any of the shape functions but leave the original data for sse and other calculations

CADETMatch/scores/curve.py
@@ -19,14 +19,20 @@ def run(sim_data, feature):
 
     exp_data_values = feature['value'][selected]
 
-    temp = [score.pear_corr(scipy.stats.pearsonr(sim_data_values, exp_data_values)[0])]
+    temp = [score.pear_corr(scipy.stats.pearsonr(sim_data_values, feature['smooth_value'])[0])]
 
     return (temp, util.sse(sim_data_values, exp_data_values), len(sim_data_values), 
             sim_time_values, sim_data_values, exp_data_values, [1.0 - i for i in temp])
 
 def setup(sim, feature, selectedTimes, selectedValues, CV_time, abstol, cache):
+    name = '%s_%s' % (sim.root.experiment_name,   feature['name'])
+    s, crit_fs = smoothing.find_smoothing_factors(selectedTimes, selectedValues, name, cache)
+    
     temp = {}
     temp['peak_max'] = max(selectedValues)
+    temp['smoothing_factor'] = s
+    temp['critical_frequency'] = crit_fs
+    temp['smooth_value'] = smoothing.smooth_data(selectedTimes, selectedValues, crit_fs, s)
     return temp
 
 def headers(experimentName, feature):