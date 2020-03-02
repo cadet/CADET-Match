1093898b3953ad2e5adb7fdec0b4ddc9c6500a1a
William Heymann <william.heymann@colorado.edu>
2020-03-02 17:33:08 +0100

    volume_length merged into norm_volume_length

--- CADETMatch/transform/norm_volume_length.py
+++ CADETMatch/transform/norm_volume_length.py
@@ -1,111 +1,177 @@
 import CADETMatch.util as util
 import numpy
+from CADETMatch.abstract.transform import AbstractTransform
 
-name = "norm_volume_length"
-count = 2
-count_extended = 3
+class VolumeLengthTransform(AbstractTransform):
+    @property
+    def name(self):
+        return "volume_length"
 
-def getUnit(location):
-    return location.split('/')[3]
+    @property
+    def count(self):
+        return 2
 
-def transform(parameter):
-    minVolume = parameter['minVolume']
-    maxVolume = parameter['maxVolume']
-    minLength = parameter['minLength']
-    maxLength = parameter['maxLength']
+    @property
+    def count_extended(self):
+        return 3
 
-    def trans_volume(i):
-        return (i - minVolume)/(maxVolume - minVolume)
+    def transform(self):
+        def trans_volume(i):
+            return i
 
-    def trans_length(i):
-        return (i - minLength)/(maxLength - minLength)
+        def trans_length(i):
+            return i
 
-    return [trans_volume, trans_length]
+        return [trans_volume, trans_length]
 
-def untransform(seq, cache, parameter):
-    minVolume = parameter['minVolume']
-    maxVolume = parameter['maxVolume']
-    minLength = parameter['minLength']
-    maxLength = parameter['maxLength']
+    grad_transform = transform
 
-    minValues = numpy.array([minVolume, minLength])
-    maxValues = numpy.array([maxVolume, maxLength])
+    def untransform(self, seq):
+        values = [seq[0]/seq[1], seq[1]]
+        headerValues = [values[0], values[1], values[0]*values[1]]
+        return values, headerValues
 
-    values = numpy.array(seq)
+    def grad_untransform(self, seq):
+        return self.untransform(seq)[0]
 
-    values = (maxValues - minValues) * values + minValues
+    def untransform_matrix(self, matrix):
+        values = self.untransform_matrix_inputorder(matrix)
+        values[:,0] = values[:,0] / values[:,1]
+        return values
 
-    values = [values[0]/values[1], values[1]]
-    headerValues = [values[0], values[1], values[0]*values[1]]
-    return values, headerValues
+    def untransform_matrix_inputorder(self, matrix):
+        values = numpy.zeros(matrix.shape)
+        values[:,0] = matrix[:,0]
+        values[:,1] = matrix[:,1]
+        return values
 
-def untransform_matrix(matrix, cache, parameter):
-    values = untransform_matrix_inputorder(matrix, cache, parameter)
-    values[:,0] = values[:,0] / values[:,1]
-    return values
-
-def untransform_matrix_inputorder(matrix, cache, parameter):
-    minVolume = parameter['minVolume']
-    maxVolume = parameter['maxVolume']
-    minLength = parameter['minLength']
-    maxLength = parameter['maxLength']
-
-    minValues = numpy.array([minVolume, minLength])
-    maxValues = numpy.array([maxVolume, maxLength])
-
-    values = (maxValues - minValues) * matrix + minValues
-
-    return values
-
-def setSimulation(sim, parameter, seq, cache, experiment):
-    values, headerValues = untransform(seq, cache, parameter)
+    def setSimulation(self, sim, seq, experiment):
+        values, headerValues = self.untransform(seq)
     
-    if parameter.get('experiments', None) is None or experiment['name'] in parameter['experiments']:
-        area_location = parameter['area_location']
-        length_location = parameter['length_location']
+        if self.parameter.get('experiments', None) is None or experiment['name'] in self.parameter['experiments']:
+            area_location = self.parameter['area_location']
+            length_location = self.parameter['length_location']
 
-        sim[area_location.lower()] = values[0]
-        sim[length_location.lower()] = values[1]
+            sim[area_location.lower()] = values[0]
+            sim[length_location.lower()] = values[1]
 
-    return values, headerValues
+        return values, headerValues
 
-def setupTarget(parameter):
-    area_location = parameter['area_location']
-    length_location = parameter['length_location']
-    bound = parameter['bound']
-    comp = parameter['component']
+    def setupTarget(self):
+        area_location = self.parameter['area_location']
+        length_location = self.parameter['length_location']
+        bound = -1
+        comp = -1
 
-    sensitivityOk = 1
-    nameArea = area_location.rsplit('/', 1)[-1]
-    nameLength = length_location.rsplit('/', 1)[-1]
-    unit = int(area_location.split('/')[3].replace('unit_', ''))
+        sensitivityOk = 1
+        nameArea = area_location.rsplit('/', 1)[-1]
+        nameLength = length_location.rsplit('/', 1)[-1]
+        unit = int(area_location.split('/')[3].replace('unit_', ''))
 
-    return [(nameArea, unit, comp, bound), (nameLength, unit, comp, bound)], sensitivityOk
+        return [(nameArea, unit, comp, bound), (nameLength, unit, comp, bound)], sensitivityOk
 
-def getBounds(parameter):
-    return [0.0, 0.0], [1.0, 1.0]
+    def getBounds(self):
+        minVolume = self.parameter['minVolume']
+        maxVolume = self.parameter['maxVolume']
+        minLength = self.parameter['minLength']
+        maxLength = self.parameter['maxLength']
 
-def getHeaders(parameter):
-    bound = parameter['bound']
-    comp = parameter['component']
+        minValues = [minVolume, minLength]
+        maxValues = [maxVolume, maxLength]
+
+        return minValues, maxValues
+
+    def getGradBounds(self):
+        return self.getBounds()
+
+    def getHeaders(self):
+        bound = -1
+        comp = -1
     
-    headers = []
-    headers.append("Area Comp:%s Bound:%s" % (comp, bound))
-    headers.append("Length Comp:%s Bound:%s" % (comp, bound))
-    headers.append("Volume Comp:%s Bound:%s" % (comp, bound))
-    return headers
+        headers = []
+        headers.append("Area Comp:%s Bound:%s" % (comp, bound))
+        headers.append("Length Comp:%s Bound:%s" % (comp, bound))
+        headers.append("Volume Comp:%s Bound:%s" % (comp, bound))
+        return headers
 
-def getHeadersActual(parameter):
-    bound = parameter['bound']
-    comp = parameter['component']
+    def getHeadersActual(self):
+        bound = -1
+        comp = -1
     
-    headers = []
-    headers.append("Volume Comp:%s Bound:%s" % (comp, bound))
-    headers.append("Length Comp:%s Bound:%s" % (comp, bound))
-    return headers
+        headers = []
+        headers.append("Volume Comp:%s Bound:%s" % (comp, bound))
+        headers.append("Length Comp:%s Bound:%s" % (comp, bound))
+        return headers
 
-def setBounds(parameter, lb, ub):
-    parameter['minVolume'] = lb[2]
-    parameter['maxVolume'] = ub[2]
-    parameter['minLength'] = lb[1]
-    parameter['maxLength'] = ub[1]
+    def setBounds(self, parameter, lb, ub):
+        parameter['minVolume'] = lb[2]
+        parameter['maxVolume'] = ub[2]
+        parameter['minLength'] = lb[1]
+        parameter['maxLength'] = ub[1]
+
+class NormVolumeLengthTransform(VolumeLengthTransform):
+    @property
+    def name(self):
+        return "norm_volume_length"
+
+    def transform(self):
+        minVolume = self.parameter['minVolume']
+        maxVolume = self.parameter['maxVolume']
+        minLength = self.parameter['minLength']
+        maxLength = self.parameter['maxLength']
+
+        def trans_volume(i):
+            return (i - minVolume)/(maxVolume - minVolume)
+
+        def trans_length(i):
+            return (i - minLength)/(maxLength - minLength)
+
+        return [trans_volume, trans_length]
+
+    grad_transform = transform
+
+    def untransform(self, seq):
+        minVolume = self.parameter['minVolume']
+        maxVolume = self.parameter['maxVolume']
+        minLength = self.parameter['minLength']
+        maxLength = self.parameter['maxLength']
+
+        minValues = numpy.array([minVolume, minLength])
+        maxValues = numpy.array([maxVolume, maxLength])
+
+        values = numpy.array(seq)
+
+        values = (maxValues - minValues) * values + minValues
+
+        values = [values[0]/values[1], values[1]]
+        headerValues = [values[0], values[1], values[0]*values[1]]
+        return values, headerValues
+
+    def grad_untransform(self, seq):
+        return self.untransform(seq)[0]
+
+    def untransform_matrix(self, matrix):
+        values = self.untransform_matrix_inputorder(matrix)
+        values[:,0] = values[:,0] / values[:,1]
+        return values
+
+    def untransform_matrix_inputorder(self, matrix):
+        minVolume = self.parameter['minVolume']
+        maxVolume = self.parameter['maxVolume']
+        minLength = self.parameter['minLength']
+        maxLength = self.parameter['maxLength']
+
+        minValues = numpy.array([minVolume, minLength])
+        maxValues = numpy.array([maxVolume, maxLength])
+
+        values = (maxValues - minValues) * matrix + minValues
+
+        return values
+
+    def getBounds(self):
+        return [0.0, 0.0], [1.0, 1.0]
+
+    def getGradBounds(self):
+        return [self.parameter['min'],], [self.parameter['max'],]
+
+plugins = {"norm_volume_length": NormVolumeLengthTransform, "volume_length": VolumeLengthTransform}
