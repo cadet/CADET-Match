573f3a96012ae9e396d8ca8f8a77464b2dede602
William Heymann <william.heymann@colorado.edu>
2020-08-05 10:46:50 +0200

    auto and auto_keq scores added These scores automatically switch between log transform and linear transform based on the min and max values, the default switch point is a max/min > 1e3 but this can be changed by setting maxFactor for auto_keq   ka and keq can switch independently
    These should be the new default transforms used since they will automatically do the right thing on using a log transform vs linear transform.
    
    These transforms also increase MCMC convergence in initial tests which is why they where created. Often the initial search range spans many orders of magnitude and a log transform is used but ones the bounds are found and narrowed a log transform was kept even though it was no longer necessary and a log transform seems to slow down mcmc convergence so it should only be used when actually necessary. On some simple problems this change about doubled MCMC performance.

---
+++ CADETMatch/transform/auto.py
@@ -0,0 +1,188 @@
+import CADETMatch.util as util
+from CADETMatch.abstract.transform import AbstractTransform
+import multiprocessing
+import numpy
+
+
+class AutoTransform(AbstractTransform):
+    @property
+    def name(self):
+        return "auto"
+
+    @property
+    def count(self):
+        return 1
+
+    @property
+    def count_extended(self):
+        return self.count
+
+    @property
+    def okay_linear(self):
+        minValue = self.parameter["min"]
+        maxValue = self.parameter["max"]
+        maxFactor = self.parameter.get('maxFactor', 1000)
+
+        return maxValue/minValue < maxFactor
+
+    @property
+    def okay_log(self):
+        return not self.okay_linear
+
+    def transform(self):
+        if self.okay_log:
+            return self.transform_log()
+        else:
+            return self.transform_linear()
+
+    def transform_log(self):
+        minValue = numpy.log(self.parameter["min"])
+        maxValue = numpy.log(self.parameter["max"])
+
+        def trans(i):
+            return (numpy.log(i) - minValue) / (maxValue - minValue)
+
+        return [
+            trans,
+        ]
+
+    def transform_linear(self):
+        minValue = self.parameter["min"]
+        maxValue = self.parameter["max"]
+
+        def trans(i):
+            return (i - minValue) / (maxValue - minValue)
+
+        return [
+            trans,
+        ]
+
+    grad_transform = transform
+
+    def untransform(self, seq):
+        if self.okay_log:
+            return self.untransform_log(seq)
+        else:
+            return self.untransform_linear(seq)
+
+    def untransform_log(self, seq):
+        minValue = numpy.log(self.parameter["min"])
+        maxValue = numpy.log(self.parameter["max"])
+
+        values = [
+            (maxValue - minValue) * seq[0] + minValue,
+        ]
+
+        values = [
+            numpy.exp(values[0]),
+        ]
+        headerValues = values
+        return values, headerValues
+
+    def untransform_linear(self, seq):
+        minValue = self.parameter["min"]
+        maxValue = self.parameter["max"]
+
+        values = [
+            (maxValue - minValue) * seq[0] + minValue,
+        ]
+        headerValues = values
+        return values, headerValues
+
+    def grad_untransform(self, seq):
+        return self.untransform(seq)[0]
+
+    def untransform_matrix(self, matrix):
+        if self.okay_log:
+            return self.untransform_matrix_log(matrix)
+        else:
+            return self.untransform_matrix_linear(matrix)
+
+    def untransform_matrix_log(self, matrix):
+        minValue = numpy.log(self.parameter["min"])
+        maxValue = numpy.log(self.parameter["max"])
+
+        temp = (maxValue - minValue) * matrix + minValue
+
+        values = numpy.exp(temp)
+
+        return values
+
+    def untransform_matrix_linear(self, matrix):
+        minValue = self.parameter["min"]
+        maxValue = self.parameter["max"]
+
+        values = (maxValue - minValue) * matrix + minValue
+        return values
+
+    untransform_matrix_inputorder = untransform_matrix
+
+    def setSimulation(self, sim, seq, experiment):
+        values, headerValues = self.untransform(seq)
+
+        if self.parameter.get("experiments", None) is None or experiment["name"] in self.parameter["experiments"]:
+            location = self.parameter["location"]
+
+            try:
+                comp = self.parameter["component"]
+                bound = self.parameter["bound"]
+                index = None
+            except KeyError:
+                index = self.parameter["index"]
+                bound = None
+
+            if bound is not None:
+                unit = self.getUnit(location)
+                boundOffset = util.getBoundOffset(sim.root.input.model[unit])
+
+                if comp == -1:
+                    position = ()
+                    sim[location.lower()] = values[0]
+                else:
+                    position = boundOffset[comp] + bound
+                    sim[location.lower()][position] = values[0]
+
+            if index is not None:
+                sim[location.lower()][index] = values[0]
+            return values, headerValues
+
+    def getBounds(self):
+        return [0.0,], [1.0,]
+
+    def getGradBounds(self):
+        return [self.parameter["min"],], [self.parameter["max"],]
+
+    def getHeaders(self):
+        if self.okay_log:
+            multiprocessing.get_logger().info("parameter %s log", self.parameter['location'])
+        else:
+            multiprocessing.get_logger().info("parameter %s linear", self.parameter['location'])
+
+        location = self.parameter["location"]
+
+        try:
+            comp = self.parameter["component"]
+        except KeyError:
+            comp = "None"
+
+        name = location.rsplit("/", 1)[-1]
+        bound = self.parameter.get("bound", None)
+        index = self.parameter.get("index", None)
+
+        headers = []
+        if bound is not None:
+            headers.append("%s Comp:%s Bound:%s" % (name, comp, bound))
+        elif index is not None:
+            headers.append("%s Comp:%s Index:%s" % (name, comp, index))
+        return headers
+
+    def getHeadersActual(self):
+        return self.getHeaders()
+
+    def setBounds(self, parameter, lb, ub):
+        parameter["min"] = lb[0]
+        parameter["max"] = ub[0]
+
+
+plugins = {"auto": AutoTransform}
+
---
+++ CADETMatch/transform/auto_keq.py
@@ -0,0 +1,196 @@
+import CADETMatch.util as util
+import numpy
+from CADETMatch.abstract.transform import AbstractTransform
+import multiprocessing
+
+class AutoKeqTransform(AbstractTransform):
+    @property
+    def name(self):
+        return "auto_keq"
+
+    @property
+    def count(self):
+        return 2
+
+    @property
+    def count_extended(self):
+        return 3
+
+    @property
+    def okay_linear_ka(self):
+        minValue = self.parameter["minKA"]
+        maxValue = self.parameter["maxKA"]
+        maxFactor = self.parameter.get('maxFactor', 1000)
+
+        return maxValue/minValue < maxFactor
+
+    @property
+    def okay_log_ka(self):
+        return not self.okay_linear_ka
+
+    @property
+    def okay_linear_keq(self):
+        minValue = self.parameter["minKEQ"]
+        maxValue = self.parameter["maxKEQ"]
+        maxFactor = self.parameter.get('maxFactor', 1000)
+
+        return maxValue/minValue < maxFactor
+
+    @property
+    def okay_log_keq(self):
+        return not self.okay_linear_keq
+
+    def transform(self):
+        minKA = self.parameter["minKA"]
+        maxKA = self.parameter["maxKA"]
+        minKEQ = self.parameter["minKEQ"]
+        maxKEQ = self.parameter["maxKEQ"]
+
+        if self.okay_log_ka:
+            def trans_a(i):
+                return (numpy.log(i) - numpy.log(minKA)) / (numpy.log(maxKA) - numpy.log(minKA))
+        else:
+            def trans_a(i):
+                return (i - minKA) / (maxKA - minKA)
+
+        if self.okay_log_keq:
+            def trans_b(i):
+                return (numpy.log(i) - numpy.log(minKEQ)) / (numpy.log(maxKEQ) - numpy.log(minKEQ))
+        else:
+            def trans_b(i):
+                return (i - minKEQ) / (maxKEQ - minKEQ)
+
+        return [trans_a, trans_b]
+
+    grad_transform = transform
+
+    def untransform(self, seq):
+        minKA = self.parameter["minKA"]
+        maxKA = self.parameter["maxKA"]
+        minKEQ = self.parameter["minKEQ"]
+        maxKEQ = self.parameter["maxKEQ"]
+
+        if self.okay_log_ka:
+            ka = (numpy.log(maxKA) - numpy.log(minKA)) * seq[0] + numpy.log(minKA)
+            ka = numpy.exp(ka)
+        else:
+            ka = (maxKA - minKA) * seq[0] + minKA
+
+        if self.okay_log_keq:
+            keq = (numpy.log(maxKEQ) - numpy.log(minKEQ)) * seq[1] + numpy.log(minKEQ)
+            keq = numpy.exp(keq)
+        else:
+            keq = (maxKEQ - minKEQ) * seq[0] + minKEQ
+
+        kd = ka/keq
+
+        values = [ka, kd]
+        headerValues = [ka, kd, keq]
+        return values, headerValues
+
+    def grad_untransform(self, seq):
+        return self.untransform(seq)[0]
+
+    def untransform_matrix(self, matrix):
+        values = self.untransform_matrix_inputorder(matrix)
+        values[:, 1] = values[:, 0] / values[:, 1]
+        return values
+
+    def untransform_matrix_inputorder(self, matrix):
+        minKA = self.parameter["minKA"]
+        maxKA = self.parameter["maxKA"]
+        minKEQ = self.parameter["minKEQ"]
+        maxKEQ = self.parameter["maxKEQ"]
+
+        if self.okay_log_ka:
+            minKA = numpy.log(minKA)
+            maxKA = numpy.log(maxKA)
+
+        if self.okay_log_keq:
+            minKEQ = numpy.log(minKEQ)
+            maxKEQ = numpy.log(maxKEQ)
+
+        minValues = numpy.array([minKA, minKEQ])
+        maxValues = numpy.array([maxKA, maxKEQ])
+
+        values = (maxValues - minValues) * matrix + minValues
+
+        if self.okay_log_ka and self.okay_log_keq:
+            values = numpy.exp(values)
+        elif self.okay_log_ka and self.okay_linear_keq:
+            values[:,0] = numpy.exp(values[:,0])
+        elif self.okay_linear_ka and self.okay_log_keq:
+            values[:,1] = numpy.exp(values[:,1])
+
+        return values
+
+    def setSimulation(self, sim, seq, experiment):
+        values, headerValues = self.untransform(seq)
+
+        if self.parameter.get("experiments", None) is None or experiment["name"] in self.parameter["experiments"]:
+            location = self.parameter["location"]
+
+            comp = self.parameter["component"]
+            bound = self.parameter["bound"]
+
+            unit = self.getUnit(location[0])
+            boundOffset = util.getBoundOffset(sim.root.input.model[unit])
+
+            position = boundOffset[comp] + bound
+            sim[location[0].lower()][position] = values[0]
+            sim[location[1].lower()][position] = values[1]
+
+        return values, headerValues
+
+    def getBounds(self):
+        return [0.0, 0.0], [1.0, 1.0]
+
+    def getGradBounds(self):
+        return [self.parameter["min"],], [self.parameter["max"],]
+
+    def getHeaders(self):
+        if self.okay_log_ka:
+            message_ka = "log ka"
+        else:
+            message_ka = "linear ka"
+
+        if self.okay_log_keq:
+            message_keq = "log keq"
+        else:
+            message_keq = "linear keq"
+
+        multiprocessing.get_logger().info("parameter %s %s %s", self.parameter['location'], message_ka, message_keq)
+
+        location = self.parameter["location"]
+        nameKA = location[0].rsplit("/", 1)[-1]
+        nameKD = location[1].rsplit("/", 1)[-1]
+        bound = self.parameter["bound"]
+        comp = self.parameter["component"]
+
+        headers = []
+        headers.append("%s Comp:%s Bound:%s" % (nameKA, comp, bound))
+        headers.append("%s Comp:%s Bound:%s" % (nameKD, comp, bound))
+        headers.append("%s/%s Comp:%s Bound:%s" % (nameKA, nameKD, comp, bound))
+        return headers
+
+    def getHeadersActual(self):
+        location = self.parameter["location"]
+        nameKA = location[0].rsplit("/", 1)[-1]
+        nameKD = location[1].rsplit("/", 1)[-1]
+        bound = self.parameter["bound"]
+        comp = self.parameter["component"]
+
+        headers = []
+        headers.append("%s Comp:%s Bound:%s" % (nameKA, comp, bound))
+        headers.append("%s/%s Comp:%s Bound:%s" % (nameKA, nameKD, comp, bound))
+        return headers
+
+    def setBounds(self, parameter, lb, ub):
+        parameter["minKA"] = lb[0]
+        parameter["maxKA"] = ub[0]
+        parameter["minKEQ"] = lb[2]
+        parameter["maxKEQ"] = ub[2]
+
+
+plugins = {"auto_keq": AutoKeqTransform}
+
